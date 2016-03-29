d98042cfa41b9d672368d766fc12872693cae2b8
mik94 <mik92@pitt.edu>
2016-03-28 23:55:05 -0400

    Final Commit

--- template/bonus_one.js
+++ template/bonus_one.js
@@ -11,7 +11,23 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
-(function() {
+(function (){
 	// Magic!
+	$(".flexsearch-input").on("input", function(){
+		$.getJSON("http://www.mattbowytz.com/simple_api.json", {data: "all"}, function(data){
+			var allData = $.merge(data.data.interests, data.data.programming);
+			$(".results").empty();
+			for (i = 0; i < allData.length; i++)
+			{
+				if (allData[i].toLowerCase().includes($(".flexsearch-input").val()))
+				{
+					var keyword = allData[i].split(" ");
+					if (keyword.length > 1) //keyword contains multiple words
+						keyword[0] += "+" + keyword[1];
+					$('.results').append("<li><a target='_blank' href='https://www.google.com/search?q=" + keyword[0] + "'>" + allData[i] + "</a></li>");
+				}
+			}
+		});
+	});
 	console.log('Keepin\'n it clean with an external script!');
 })();
